76b1aa77da967b061b1a4dabcc1f1ebaecd9a448
Thommynator <T.SchindlerMail@gmail.com>
2018-02-24 21:02:13 +0100

    Added visualization of measurement arc area.

--- src/Robot.js
+++ src/Robot.js
@@ -4,15 +4,17 @@ function Robot(xPos, yPos, heading) {
     this.heading = heading
     this.w = 20
     this.h = 40
+    this.sensorRange = 200.0
+
     var nSensors = 9
     this.dist = Array(nSensors).fill(0.0)
 
-    this.moveForward = function() {
-        this.x += sin(this.heading)
-        this.y -= cos(this.heading)
+    this.moveForward = function () {
+        this.x += sin(this.heading) * 2
+        this.y -= cos(this.heading) * 2
     }
 
-    this.moveBackward = function() {
+    this.moveBackward = function () {
         this.x -= sin(this.heading)
         this.y += cos(this.heading)
     }
@@ -21,32 +23,36 @@ function Robot(xPos, yPos, heading) {
 
     // }
 
-    this.update = function() {
-        if(keyIsDown(UP_ARROW)) {
+    this.update = function () {
+        if (keyIsDown(UP_ARROW)) {
             robot.moveForward()
-        } 
-        if(keyIsDown(DOWN_ARROW)) {
+        }
+        if (keyIsDown(DOWN_ARROW)) {
             robot.moveBackward()
-        } 
-        if(keyIsDown(LEFT_ARROW)) {
+        }
+        if (keyIsDown(LEFT_ARROW)) {
             robot.heading -= 0.05
-        } 
-        if(keyIsDown(RIGHT_ARROW)) {
+        }
+        if (keyIsDown(RIGHT_ARROW)) {
             robot.heading += 0.05
         }
     }
 
-    this.show = function() {
-        var icon = createGraphics(100, 100)
+    this.show = function () {
         push()
-        icon.background(220)
         translate(this.x, this.y)
         rotate(this.heading)
-        icon.fill(100, 100, 255)
-        icon.rect(0, 0, this.w, this.h)
-        icon.strokeWeight(2)
-        icon.ellipse(this.w/2, this.h/4, 10, 10)
-        image(icon, -this.w/2, -this.h/2)
+
+        fill(255, 30)
+        var startAngle = PI + QUARTER_PI
+        arc(0, -this.w / 2, this.sensorRange, this.sensorRange, startAngle, startAngle + HALF_PI)
+
+        fill(100, 100, 255)
+        rectMode(CENTER)
+        rect(0, 0, this.w, this.h)
+        strokeWeight(2)
+        ellipseMode(CENTER)
+        ellipse(0, -this.w / 2, 10, 10)
         pop()
     }
 }
